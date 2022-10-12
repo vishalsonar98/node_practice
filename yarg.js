e035c85631b9fc6c51c06d898fa47849272eb2dd
vishalsonar98 <vishalsonar2012@gmail.com>
2022-10-12 18:04:06 +0530

    12/10/2022 1

--- yarg.js
+++ yarg.js
@@ -4,17 +4,43 @@ const yargs = require("yargs");
 yargs.command({
     command:'remove',
     describe:'remove note',
-    handler:function () {
-        console.log("removing note")
+    builder:{
+        title:{
+            describe:'Note Title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Body of note',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function (argv) {
+        console.log("removing note",argv)
     }
 })
 yargs.command({
     command:'add',
     describe:'add note',
-    handler:function () {
-        console.log("adding note")
+    builder:{
+        title:{
+            describe:'Note Title',
+            demandOption:true,
+            type:'string'
+        },
+        body:{
+            describe:'Body of note',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function (argv) {
+        console.log("title: " + argv.title)
+        console.log("body: " + argv.body)
     }
 })
+
 
 yargs.command({
     command:'read',
@@ -30,6 +56,6 @@ yargs.command({
         console.log("listing note")
     }
 })
-console.log(yargs.argv)
+// console.log(yargs.argv)
 // console.log(process.argv)
-
+yargs.parse()
